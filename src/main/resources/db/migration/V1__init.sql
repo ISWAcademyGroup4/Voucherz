5f4f15eccb49020992247b826c2dc417db11fd20
Humphreychinedu <mefendja.chinedu@gmail.com>
2019-01-23 08:12:02 +0100

    Update V1__init.sql

--- src/main/resources/db/migration/V1__init.sql
+++ src/main/resources/db/migration/V1__init.sql
@@ -61,3 +61,25 @@ AS
     SELECT * FROM [dbo].[Users] WHERE Email = @Email
 
     RETURN @@Error
+
+GO
+ALTER PROCEDURE [dbo].[uspUserUpdate]
+@id BIGINT,
+@firstname NVARCHAR(50) = NULL,
+@lastname NVARCHAR(50) = NULL,
+@password NVARCHAR(50) = NULL,
+@email NVARCHAR(50) = NULL,
+@phonenumber NVARCHAR(50) = NULL,
+@companysize INT = NULL
+
+AS
+BEGIN
+	-- SET NOCOUNT ON added to prevent extra result sets from
+	-- interfering with SELECT statements.
+	SET NOCOUNT ON;
+
+UPDATE Users
+SET FirstName = isNULL(@firstname,FirstName ), LastName = isNuLL(@lastname,LastName), [Password] = isNULL(@password,[Password]), Email = isNULL(@email,Email), PhoneNumber = isNULL(@phonenumber,PhoneNumber), CompanySize = isNULL(@companysize,CompanySize)
+WHERE (UserId = @id)
+END
+GO
