28a39d2245745dfe431fbb7ec16cb1f44793c153
Humphreychinedu <mefendja.chinedu@gmail.com>
2019-01-23 07:04:47 +0100

    Initial commit

---
+++ src/main/java/com/iswAcademy/Voucherz/Model/UpdateUserRequest.java
@@ -0,0 +1,6 @@
+package com.iswAcademy.Voucherz.Model;
+
+import com.iswAcademy.Voucherz.Api.Controller.Model.UserRegistrationRequest;
+
+public class UpdateUserRequest extends UserRegistrationRequest {
+}
